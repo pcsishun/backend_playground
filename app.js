cef18d4dab349b7449644a0be88144e0263efdfd
pcsishun <pcsishun@gmail.com>
2022-04-01 15:45:55 +0700

    update

--- app.js
+++ app.js
@@ -8,7 +8,27 @@ app.use(express.json());
 app.use(cors()); 
 
 app.post("/userplayground", async (req, res) => {
+    const { username,  password } = req.body;
+    try{
+        const userProfile  = await UserPlayground.findOne({username});
+        
+        if(userProfile && (userProfile.password === password)){
+            const replyText = {
+                status: true, 
+                text: userProfile
+            }
+            res.send(replyText)
+        }else{
+            
+        }
 
+    }catch(err){
+        const replyText ={
+            status: false, 
+            text:"not found this username"
+        }
+        res.send(replyText)
+    }
 });
 
 
@@ -26,7 +46,18 @@ app.post("/register", async (req, res) => {
             const oldUser = await UserPlayground.findOne({username});
 
             if(!oldUser){
-                
+                await User.create({
+                    username: username,
+                    firstname: firstname,
+                    lastname: lastname,
+                    password: password,
+                })
+
+                const replyText = {
+                    registerStatus: true, 
+                    text: "register success."
+                }
+                res.status(200).send(replyText)
             }else{
                 const replyText = {
                     registerStatus: false,
